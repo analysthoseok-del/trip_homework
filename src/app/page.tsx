8a29176bd9df3ebe76048ddcae3c8d1f91d7952b
analysthoseok-del <analysthoseok@gmail.com>
2026-08-24 15:36:45 +0900

    업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>
-            To get started, edit the{" "}
+            안녕하세요 새롱누 홈페이지입니다{" "}
             <code className={styles.code}>page.tsx</code> file.
           </h1>
           <p>
